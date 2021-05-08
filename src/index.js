7ed833b23516aaba9f22d0aa68f2ead18dfbcacc
Ed Hansen <ed@neubra.dk>
2021-05-08 21:42:24 +0200

    props id

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css'
 import Card from './Card.js'
 import reportWebVitals from './reportWebVitals'
 import 'tachyons'
-s
+import { robots } from './robots'
+
 ReactDOM.render(
   <React.StrictMode>
-    <Card />
+    <>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 )
