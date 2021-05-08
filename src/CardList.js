d5b32700037c3683d02598c76d8d467fa4921040
Ed Hansen <ed@neubra.dk>
2021-05-08 22:05:51 +0200

    card in place

---
+++ src/CardList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+
+const CardList = ({ robots }) => {
+  const cardArrays = robots.map((user, i) => {
+    return (
+      <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    )
+  })
+
+  return <div>{cardArrays}</div>
+}
+
+export default CardList
