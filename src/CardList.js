955997e4806f0abfd83a9fd6ec84d8849d507f89
Ed Hansen <ed@neubra.dk>
2021-05-14 17:10:35 +0200

    cardlist return within div

--- src/CardList.js
+++ src/CardList.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Card from './Card'
 
 const CardList = ({ robots }) => {
-  const cardArrays = robots.map((user, i) => {
-    return (
-      <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-    )
-  })
-
-  return <div>{cardArrays}</div>
+  return (
+    <div>
+      {robots.map((_user, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        )
+      })}
+    </div>
+  )
 }
 
 export default CardList
