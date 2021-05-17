43c13a474e5f2ec636cf29b2ac31559d5c2ffa85
Ed Hansen <ed@neubra.dk>
2021-05-17 20:01:39 +0200

    api fetch details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
+
+import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users').then((respnse) =>
+      respnse.json().then((users) => this.setState({ robots: users }))
+    )
   }
 
   onSearchChange = (event) => {
@@ -22,14 +29,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase())
     })
-
-    return (
-      <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobot} />
-      </div>
-    )
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobot} />
+        </div>
+      )
+    }
   }
 }
 
