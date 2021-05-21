072fcf4b1240fa8f0554f698d206a5ecff0e14de
Ed Hansen <ed@neubra.dk>
2021-05-21 15:05:26 +0200

    Errorboudry added to cardlist demo

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,13 @@ import React from 'react'
 import Card from './Card'
 
 const CardList = ({ robots }) => {
+  /**
+   * Simulate error
+   *
+ * if (true) {
+    throw new Error('Noooooo')
+  }
+ * */
   return (
     <div>
       {robots.map((_user, i) => {
