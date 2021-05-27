83410bea98b1b8decdee41ed4ab607371b158b5c
Ed Hansen <ed@neubra.dk>
2021-05-27 21:27:02 +0200

    redux added

--- src/container/App.js
+++ src/container/App.js
@@ -10,18 +10,16 @@ function App() {
   // Declare a new state variable, which we'll call "field"
   const [robots, setRobots] = useState([])
   const [searchfield, setSearchfield] = useState('')
-  const [count, setCount] = useState(0)
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users').then((respnse) =>
       respnse.json().then((users) => {
         setRobots(users)
-        console.log(count)
       })
     )
     // console.log(robots, searchfield)
-  }, [count]) //Only run if ccount changes
+  }, []) //Only run if count changes
 
   // Need to turn this into a variable
   const onSearchChange = (event) => {
@@ -37,7 +35,6 @@ function App() {
   ) : (
     <div className='tc'>
       <h1 className='f1'>RoboFriends</h1>
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
       <SearchBox searchChange={onSearchChange} />
       <Scroll>
         <ErrorBoundry>
